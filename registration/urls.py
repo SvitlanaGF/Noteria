b086b4c345677fdbd8b8ff6c58689c7531c6035c
Svitlana <svitlanagayduchyk76@gmail.com>
2023-09-02 15:38:35 +0300

    register and login

---
+++ registration/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path, include
+from . import views
+
+urlpatterns = [
+    path('', views.main, name="main"),
+    path('login/', views.login, name="login"),
+    path('register/', views.register, name="register")
+]
+
---
+++ registration/views.py
@@ -0,0 +1,36 @@
+from django.shortcuts import render, redirect, HttpResponseRedirect
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
+
+def main(request):
+    return render(request, 'start.html')
+
+
+def login(request):
+    if request.method == 'POST':
+        usname = request.POST.get("us_name")
+        email = request.POST.get("us_email")
+        password_ = request.POST.get("name-password")
+        usr = authenticate(request, username=usname,email=email,password=password_)
+        if usr is not None:
+            login(request, usr)
+            return HttpResponseRedirect('Success!')
+    return render(request, 'login.html')
+
+
+def register(request):
+    if request.method == 'POST':
+        usname = request.POST.get("us_name")
+        email = request.POST.get("us_email")
+        password_ = request.POST.get("name-password")
+        password_test = request.POST.get("name-password1")
+        if password_ != password_test:
+            return HttpResponseRedirect("Wrong password")
+        usr = User.objects.create_user(username=usname, email=email, password=password_)
+        usr.save()
+        return redirect('login')
+    return render(request, 'register.html')
+
+def Logout(request):
+    logout(request)
+    return request('main')
