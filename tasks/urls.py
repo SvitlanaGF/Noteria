8bfd430785b989f609bce147fe7c9d9e937c3902
Svitlana <svitlanagayduchyk76@gmail.com>
2023-09-05 00:24:56 +0300

    Delete

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path, include
-from .views import ListOfNotes, Notice, CreateNotice
+from .views import ListOfNotes, Notice, CreateNotice, UpdateNotice, DeleteNotice
 
 
 urlpatterns = [
     path('notes/', ListOfNotes.as_view(), name="notes"),
     path('notice/<int:pk>/', Notice.as_view(), name="notice"),
-    path('create-notice/', CreateNotice.as_view(), name="create-notice")
+    path('create-notice/', CreateNotice.as_view(), name="create-notice"),
+    path('notice-edit/<int:pk>/', UpdateNotice.as_view(), name="notice-edit"),
+    path('notice-delete/<int:pk>/', DeleteNotice.as_view(), name="notice-delete")
 ]
 
--- tasks/views.py
+++ tasks/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, HttpResponseRedirect
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
 from .models import Notation
 
@@ -21,3 +21,14 @@ class CreateNotice(CreateView):
     fields = '__all__'
     success_url = reverse_lazy("notes")
 #     # context_object_name = "create-notice"
+
+
+class UpdateNotice(UpdateView):
+    model = Notation
+    fields = '__all__'
+    success_url = reverse_lazy("notes")
+
+class DeleteNotice(DeleteView):
+    model = Notation
+    context_object_name = "notice"
+    success_url = reverse_lazy("notes")
